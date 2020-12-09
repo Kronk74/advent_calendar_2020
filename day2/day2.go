f59b64b609a2651b2479907b03aaf9e328f70b2b
Thomas RAMON <ramon.thomas.74@gmail.com>
2020-12-09 21:10:09 +0100

    resolve day2 + remove exec files

--- day2/day2.go
+++ day2/day2.go
@@ -32,19 +32,36 @@ func part2(file string) int {
 	for i := 0; i < len(input); i++ {
 		s := strings.Split(input[i], " ")
 		nr := strings.Split(s[0], "-")
-		var test int = strings.Count(s[2], strings.Trim(s[1], ":"))
+		letter := strings.Trim(s[1], ":")
+		st := strings.Split(s[2], "")
 		min, _ := strconv.Atoi(nr[0])
 		max, _ := strconv.Atoi(nr[1])
-		if (min <= test) && test <= max {
+		var j []int
+		var m int = 0
+		for i := 0; i < len(st); i++ {
+			if st[i] == letter {
+				j = append(j, i+1)
+			}
+		}
+		for i := 0; i < len(j); i++ {
+			if min == j[i] {
+				m++
+			}
+			if max == j[i] {
+				m++
+			}
+		}
+		if m == 1 {
 			validpass++
 		} else {
 			errorpass++
 		}
 	}
+
 	return validpass
 }
 
 func main() {
-	validpass := part1("input")
+	validpass := part2("input")
 	fmt.Println(validpass)
 }
